65e45a707adda05234667a1ae30007ad85c38975
Guilherme wsl <guilhergoa@hotmail.com>
2024-06-11 20:10:39 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('WhatsApp nest')
+    .setDescription('WhatsApp to App')
+    .setVersion('1.0')
+    .addTag('wpp')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+  await app.listen(3000);
+}
+bootstrap();
